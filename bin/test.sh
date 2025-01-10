2da7ed333a4f58f648bb7605f1a9a06264de2d5e
Hekepoiu <decnovpk92805@gmail.com>
2025-01-11 07:48:57 +0800

    Fix Test Data, and imporve test script

--- bin/test.sh
+++ bin/test.sh
@@ -2,18 +2,28 @@
 
 for i in ./test/*.hi
 do
-    if ! cabal v2-run hibiscus $i ; then
+    echo "-------------------------------------------------------"
+    echo "Start compile $i"
+    echo "--"
+    output=$(cabal v2-run hibiscus "$i" 2>&1)
+    if [ $? -ne 0 ] ; then
+        echo "$output"
         echo "error to compile $i";
         exit 1;
     fi
+    echo "Ok"
 done
 
 for i in ./test/*.hi.asm
 do
+    echo "-------------------------------------------------------"
+    echo "Start val $i"
+    echo "--"
     spirv-as "$i" -o "./test/out.spv"
     if ! spirv-val "./test/out.spv"; then
         echo "error in $i";
         exit 1;
     fi
+    echo "Ok"
 
 done
