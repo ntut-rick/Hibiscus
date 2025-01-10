9e361fa780262ac55432552fd4d2382ece6e3d7d
Hekepoiu <decnovpk92805@gmail.com>
2025-01-11 01:11:44 +0800

    Gen asm

--- bin/test.sh
+++ bin/test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+for i in ./test/*.hi
+do
+    if ! cabal run hibiscus.cabal $i ; then
+        echo "error to compile $i";
+        exit 1;
+    fi
+done
+
 for i in ./test/*.hi.asm
 do
     spirv-as "$i" -o "./test/out.spv"
     if ! spirv-val "./test/out.spv"; then
-        echo "error in $i"
+        echo "error in $i";
         exit 1;
     fi
 
