1a1342918868835199f95d4ff837c7a06016e9da
rick <t110590016@ntut.edu.tw>
2025-01-11 13:03:42 +0800

    remove illegal test case

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+error_flag=0
+
 for i in ./test/*.hi
 do
     echo "-------------------------------------------------------"
@@ -8,10 +10,11 @@ do
     output=$(cabal v2-run hibiscus "$i" 2>&1)
     if [ $? -ne 0 ] ; then
         echo "$output"
-        echo "error to compile $i";
-        exit 1;
+        echo "error to compile $i"
+        error_flag=1
+    else
+        echo "Ok"
     fi
-    echo "Ok"
 done
 
 for i in ./test/*.hi.asm
@@ -21,9 +24,16 @@ do
     echo "--"
     spirv-as "$i" -o "./test/out.spv"
     if ! spirv-val "./test/out.spv"; then
-        echo "error in $i";
-        exit 1;
+        echo "error in $i"
+        error_flag=1
+    else
+        echo "Ok"
     fi
-    echo "Ok"
-
 done
+
+# Exit with error if any step failed
+if [ $error_flag -ne 0 ]; then
+    exit 1
+else
+    exit 0
+fi
